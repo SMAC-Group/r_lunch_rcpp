1570dff88cd9e213fbdea85f7d28abf6ef891100
Lionel Voirol <lionelvoirol@hotmail.com>
2021-04-25 11:41:06 +0200

    add wvar computation in cpp and benchmark plot

---
+++ R/haar_r.R
@@ -0,0 +1,45 @@
+#calculate wavelet coefficient with modwt coefficient
+wvar_r = function(Xt){
+  tsl = length(Xt)
+  #define J
+  J = log(tsl, 2) %>% floor()
+  #create list to store elements
+  haar_coeff_list = list()
+  all_j =   J %>% seq()
+  #define all scales
+  scales = 2^all_j
+  for(i_j in all_j){
+    i_scale = scales[i_j]
+    length_haar_transfo = tsl - 2^i_j + 1
+    
+    #define positive and negative yt
+    coef_length = seq(length_haar_transfo)
+    coef_scale_i = vector(length =length_haar_transfo, mode = "numeric")
+    for(cl in coef_length){
+      #define all position
+      pos_scale_i = seq(cl, i_scale+cl-1)
+      #define t/2 for scale j
+      mid_id = length(pos_scale_i)/2
+      #define left and right position
+      neg_id = pos_scale_i[1:mid_id] 
+      pos_id =  tail(pos_scale_i,length(pos_scale_i)/2)
+      #calculate haar coefficient define as the weighted mean where weight equal -1, 1
+      xt_neg = Xt[neg_id] * -1
+      xt_pos = Xt[pos_id]
+      xt_weighted = c(xt_neg, xt_pos)
+      coef_scale_i[cl] = mean(xt_weighted)
+    }
+    #append to growing vector
+    haar_coeff_list[[i_j]] = coef_scale_i
+  }
+  #calculate wavelet variance
+  wvariance = vector(mode = "numeric", length = length(all_j))
+  for(i in seq(length(all_j))){
+    # wvariance[i] = var(haar_coeff_list[[i]])*(length(haar_coeff_list[[i]])-1)/length(haar_coeff_list[[i]])
+    # wvariance[i] = var(haar_coeff_list[[i]])
+    haar_coef = haar_coeff_list[[i]]
+    wvariance[i] = t(haar_coef) %*% haar_coef / length(haar_coef)
+  }
+  #return haar coefficients and wvariance
+  return(wvariance)
+}
---
+++ R/rcpp_rlunch_script.R
@@ -0,0 +1,114 @@
+#########################################
+# Working with cpp in R
+#########################################
+# clean ws
+rm(list = ls())
+
+#load packages
+library(wv)
+library(simts)
+require(magrittr)
+library(microbenchmark)
+library(Rcpp)
+library(RcppArmadillo)
+
+################# measuring performance in R
+
+
+# loading functions with source cpp
+sourceCpp("R/sum_cpp.cpp")
+
+# loading function with inline cpp
+
+
+
+##################################
+# Basic exemple
+##################################
+
+
+
+
+
+##################################
+# Example Wavelet Variance
+##################################
+
+# load R implementation
+sourceCpp(file = "R/haar_cpp.cpp")
+# load cpp implementation
+source(file = "R/haar_r.R")
+
+# test equality
+#generate ts
+set.seed(12345)
+Xt = gen_gts(n=10^3, model = WN(1))
+
+# compute wvar
+wvar_r_implementation = wvar_r(Xt)
+wvar_cpp_implementation = wvar_cpp(Xt)
+wvar_package_implementation = wv::wvar(Xt)
+all.equal(as.vector(wvar_cpp_implementation), wvar_r_implementation)
+all.equal(wvar_r_implementation[1:8], wvar_package_implementation$variance)
+
+
+
+# benchmark for different signal size
+lst_benchmark = list()
+for(i in seq(4)){
+  size = 10^i
+  set.seed(12345)
+  Xt = gen_gts(n=size, model = WN(1))
+  res_benchmark_size_i = microbenchmark(
+    wvar_r(Xt),
+    wvar_cpp(Xt),
+    times = 10
+  )
+  lst_benchmark[[i]] = print(res_benchmark_size_i, unit ="us")
+  print(size)
+}
+lst_benchmark
+
+mat_mean_perf = matrix(ncol = 6, nrow= 4)
+for(i in seq(4)){
+  mat_mean_perf[i, ] = cbind(lst_benchmark[[i]][,4], lst_benchmark[[i]][,2],lst_benchmark[[i]][,7])
+}
+
+save(mat_mean_perf, file = "mat_mean_perf.rda")
+
+t_col <- function(color, percent = 90, name = NULL) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
+  
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+  
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100 - percent) * 255 / 100,
+               names = name)
+  
+  ## Save the color
+  # invisible(t.col)
+  t.col
+}
+t_col("darkblue")
+t_col("darkorange", percent = .8)
+
+par(mar=c(5,5,1,1))
+plot(x = 10^seq(4), y = mat_mean_perf[,2], 
+     log="xy", type = "b", ylim = c(1, 10e6), xlab = "Sample size",
+     ylab = "Mean execution time (microseconds)", cex.lab = 1.5, cex.main = 1.5,cex.axis = 1.3,
+     col = "darkblue", pch = 16)
+grid(col="grey80", lty=1)
+polygon(x = c(10^seq(4), rev(10^seq(4))), y = c(mat_mean_perf[,3], rev(mat_mean_perf[,5])), col = "#FF8C0019" , border = NA)
+polygon(x = c(10^seq(4), rev(10^seq(4))), y = c(mat_mean_perf[,4], rev(mat_mean_perf[,6])), col = "#00008B19", border = NA)
+lines(x = 10^seq(4), y = mat_mean_perf[,1], type ="b",  col = "darkorange", pch = 16)
+lines(x = 10^seq(4), y = mat_mean_perf[,2], type ="b",  col = "darkblue", pch = 16)
+legend("topleft",col = c("darkorange", "darkblue"), legend= c("CPP", "R"), lwd=1, pch = 16, bty ="n", cex = 1.5)
+
+
+
+
